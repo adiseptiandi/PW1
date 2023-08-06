69680382256cb1017caac10d3f829725e5dabbf0
adiseptiandi <adiseptiandi98@gmail.com>
2023-08-06 12:05:14 +0700

    Upload UAS PW1

---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('audio');
+    
+    // Fungsi untuk memainkan audio secara otomatis
+    function playAudio() {
+      audio.play();
+      // Hapus event listener agar audio tidak diputar berulang kali
+      document.removeEventListener('click', playAudio);
+    }
+    
+    // Event listener untuk memainkan audio ketika halaman dimuat sepenuhnya
+    document.addEventListener('click', playAudio);
+  });
+  
